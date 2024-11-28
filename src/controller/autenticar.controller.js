a174bc1b2fbd158cf7b9aeedf7926ef296a4c146
“Bryan4638” <brayancespedes57@gmail.com>
2024-11-28 00:33:05 -0500

    add login and verify token

--- src/controller/autenticar.controller.js
+++ src/controller/autenticar.controller.js
@@ -1,5 +1,9 @@
+const { verify } = require("jsonwebtoken");
 const pool = require("../db");
 const { createToken } = require("../libs/jwt");
+const {
+  obtenerContribuyentePorUserName,
+} = require("./contribuyente.controller");
 
 const getAutenticar = async (req, res, next) => {
   try {
@@ -63,35 +67,81 @@ const deleteAutenticar = async (req, res, next) => {
   }
 };
 
-const login = async (req, res) => {
+async function verificarCredenciales(userName, password) {
+  const query = `
+        SELECT a.user_name, a.contrasenna
+        FROM autenticar a
+        WHERE a.user_name = $1;
+    `;
+
   try {
-    const { user_name, contrasenna } = req.body;
+    const result = await pool.query(query, [userName]);
 
-    if (!user_name || !contrasenna) {
-      return res
-        .status(401)
-        .json(["Nesecita email y contraseña para logearce"]);
+    if (result.rows.length > 0) {
+      const usuario = result.rows[0];
+      /* const passwordValida = await bcrypt.compare(password, usuario.password);
+            if (passwordValida) {
+                return usuario;
+            } */
+      return usuario;
     }
-
-    //verificar q el usuario existe
-
-    if (!user) {
-      return res.status(401).json(["Usuario no encontrado"]);
-    }
-
-    const token = await createToken(String(user.id));
-
-    res.cookie("token", token, {
-      httpOnly: false,
-      secure: true,
-      sameSite: "none",
-    });
-    res.json({
-      
-    });
+    return null; // Credenciales incorrectas
   } catch (error) {
-    console.log(error);
-    res.status(500).json(["Error del servidor"]);
+    console.error("Error al verificar las credenciales:", error);
+    throw error;
+  }
+}
+
+function autenticarToken(req, res) {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(403).json({ message: "Token no proporcionado." });
+  }
+
+  jwt.verify(token, "TOKEN_SECRET", async (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Token no válido." });
+    }
+    console.log(user);
+
+    const contribuyente = await obtenerContribuyentePorUserName(user.userName);
+
+    if (contribuyente) {
+      res.status(200).json({ message: "Token válido.", contribuyente });
+    } else {
+      res.status(404).json({ message: "Contribuyente no encontrado." });
+    }
+  });
+}
+
+const login = async (req, res) => {
+  const { userName, password } = req.body;
+
+  try {
+    const usuario = await verificarCredenciales(userName, password);
+
+    const contribuyente = await obtenerContribuyentePorUserName(
+      usuario.user_name
+    );
+
+    if (usuario) {
+      const token = createToken(usuario.user_name);
+      res.cookie("token", token, {
+        httpOnly: false,
+        secure: true,
+        sameSite: "none",
+      });
+      res.json({
+        contribuyente,
+      });
+    } else {
+      res.status(401).json({ message: "Credenciales incorrectas." });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error al iniciar sesión.", error: error.message });
   }
 };
 
@@ -100,4 +150,6 @@ module.exports = {
   postAutenticar,
   putAutenticar,
   deleteAutenticar,
+  login,
+  autenticarToken,
 };
